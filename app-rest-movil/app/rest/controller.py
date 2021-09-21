a76e66e594fe12fd196e6da323822a15735e279c
davidmp <mamanipari@gmail.com>
2021-09-21 18:29:46 -0500

    crear rest

--- app-rest-movil/app/rest/controller.py
+++ app-rest-movil/app/rest/controller.py
@@ -1,6 +1,8 @@
 from flask.wrappers import Response
+from flask import request, jsonify
 import json
 from app import app,mongo 
+import datetime
 @app.route("/pru")
 def home_page():
     data = list(mongo.db.user.find())
@@ -10,6 +12,34 @@ def home_page():
         response=json.dumps(data,default=str),
         status=200,
         mimetype="application/json"
-    )
-    #return render_template("index.html",
-    #    online_users=online_users)
+    )    
+
+@app.route("/crear",methods=["POST"])
+def crear():
+    try:
+        _json=request.json
+        data_format=datetime.datetime.strptime(_json["fecha_nac"],'%Y-%m-%d')
+        _json["fecha_nac"]=data_format
+        dbResponse=mongo.db.persona.insert_one(_json)
+        print(dbResponse.inserted_id)
+        return Response(
+            response=json.dumps(
+                {
+                    "mesage":"Se creo correctamente"
+                }, default=str
+            ),
+            status=200,
+            mimetype="application/json"
+        )
+    except Exception as e:
+        print(e)
+        return Response(
+            response=json.dumps(
+                {
+                    "mesage":"Error al crear registro"
+                }, default=str
+            ),
+            status=200,
+            mimetype="application/json"
+        )
+    
